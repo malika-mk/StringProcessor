f56df8f79e034cb08a53e194c1a370d02dfc799b
Malika <malikamkkambay@gmail.com>
2024-10-06 17:16:50 +0600

    adding comments

--- src/test/java/StringProcessorTest.java
+++ src/test/java/StringProcessorTest.java
@@ -9,42 +9,42 @@ class StringProcessorTest {
     @Test
     void isStrongPassword(){
         StringProcessor stringProcessor = new StringProcessor();
-        assertTrue(stringProcessor.isStrongPassword("Password123!"));
-        assertFalse(stringProcessor.isStrongPassword("weakpass!"));
-        assertTrue(stringProcessor.isStrongPassword("Str0ng!"));
-        assertFalse(stringProcessor.isStrongPassword("malika!"));
-        assertTrue(stringProcessor.isStrongPassword("MMkadsdf123$!"));
+        assertTrue(stringProcessor.isStrongPassword("Password123!")); // Output: True
+        assertFalse(stringProcessor.isStrongPassword("weakpass!"));// Output: False
+        assertTrue(stringProcessor.isStrongPassword("Str0ng!"));// Output: True
+        assertFalse(stringProcessor.isStrongPassword("malika!"));// Output: False
+        assertTrue(stringProcessor.isStrongPassword("MMkadsdf123$!"));// Output: True
 
     }
 
     @Test
     void calculateDigits(){
         StringProcessor stringProcessor = new StringProcessor();
-        assertEquals(4, stringProcessor.calculateDigits("Hello_2024!"));
-        assertEquals(0, stringProcessor.calculateDigits("No digits here!"));
-        assertEquals(3, stringProcessor.calculateDigits("123 Main St."));
-        assertEquals(4, stringProcessor.calculateDigits("6000 y.o."));
+        assertEquals(4, stringProcessor.calculateDigits("Hello_2024!")); // Output: 4
+        assertEquals(0, stringProcessor.calculateDigits("No digits here!"));// Output: 0
+        assertEquals(3, stringProcessor.calculateDigits("123 Main St."));// Output: 3
+        assertEquals(4, stringProcessor.calculateDigits("6000 y.o."));// Output: 4
     }
 
     @Test
     void calculateWords(){
         StringProcessor stringProcessor = new StringProcessor();
-        assertEquals(1, stringProcessor.calculateWords("Hello"));
-        assertEquals(4, stringProcessor.calculateWords("  This is a sentence.  "));
-        assertEquals(0, stringProcessor.calculateWords(""));
-        assertEquals(1, stringProcessor.calculateWords("OneWord"));
-        assertEquals(2, stringProcessor.calculateWords("   Multiple    spaces  "));
+        assertEquals(1, stringProcessor.calculateWords("Hello"));// Output: 1
+        assertEquals(4, stringProcessor.calculateWords("  This is a sentence.  "));// Output: 4
+        assertEquals(0, stringProcessor.calculateWords(""));// Output: 0
+        assertEquals(1, stringProcessor.calculateWords("OneWord"));// Output: 1
+        assertEquals(2, stringProcessor.calculateWords("   Multiple    spaces  "));// Output: 2
 
     }
 
     @Test
     void calculateExpression(){
         StringProcessor stringProcessor = new StringProcessor();
-        assertEquals(22.0, stringProcessor.calculateExpression("10 + 2 * 6"));
-        assertEquals(1400.0, stringProcessor.calculateExpression("100 * ( 2 + 12 )"));
-        assertEquals(100.0, stringProcessor.calculateExpression("100 * ( 2 + 12 ) / 14"));
-        assertEquals(16.0, stringProcessor.calculateExpression("(5 + 3) * 2"));
-        assertEquals(11.0, stringProcessor.calculateExpression("1 + 2 * 5"));
+        assertEquals(22.0, stringProcessor.calculateExpression("10 + 2 * 6"));// Output: 22.0
+        assertEquals(1400.0, stringProcessor.calculateExpression("100 * ( 2 + 12 )"));// Output: 1400.0
+        assertEquals(100.0, stringProcessor.calculateExpression("100 * ( 2 + 12 ) / 14"));// Output: 100.0
+        assertEquals(16.0, stringProcessor.calculateExpression("(5 + 3) * 2"));// Output: 16.0
+        assertEquals(11.0, stringProcessor.calculateExpression("1 + 2 * 5"));// Output: 11.0
     }
 
 }
